fe1ddf331e27dbb9b690b4a3486f7f532ee1dc88
shahnizam1 <shahnizam123@gmail.com>
2017-05-12 09:44:40 +0800

    Add files via upload

---
+++ Smartphones_Apps/init.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(0);
+
+$db_name = "id1489556_android_database";
+$mysql_user = "id1489556_android";
+$mysql_pass = "software";
+$server_name = "localhost";
+
+$con = mysqli_connect($server_name, $mysql_user, $mysql_pass, $db_name);
+
+//if ($con) {
+//        echo '{"message":Connected."}';
+//}
+
+if(!$con){
+	echo '{"message":"Unable to connect to the database."}';
+}
+
+
+?>
