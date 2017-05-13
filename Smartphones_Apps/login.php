6a826d498ba64a1443d2a8151d8a222324177b50
syahmikhairudin <syahmikhairudin@gmail.com>
2017-05-13 10:58:25 +0800

    Update login.php

--- Smartphones_Apps/login.php
+++ Smartphones_Apps/login.php
@@ -2,24 +2,10 @@
 error_reporting(0);
 require "init_rpi.php";
 
-//$name = $_POST["name"];
-//$password = $_POST["password"];
 
-//$name = "www";
-//$password = "www";
-
-//$sql = "SELECT * FROM `user_info` WHERE name='".$name."' AND `password`='".$password."';";
-//$sql = "SELECT * FROM `register_1` WHERE name='$_GET[name]' AND password='$_GET[password]'";
 $sql = "SELECT * FROM `register_1` WHERE name='$_POST[name]' AND password='$_POST[password]'";
 
 
-//if(!mysqli_query($con, $sql)){
-//	echo '{"message":"Unable to get the data to the database."}';
-//}
-
-//if(mysqli_query($con, $sql)){
-//	echo '{"message":"Good."}';
-//}
 
 $result = mysqli_query($con, $sql);
 
